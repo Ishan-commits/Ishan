f2a0471e6bb448b0e8a7ea70eab10bceeb2ca8a6
Chris Little <chrisclittle@gmail.com>
2017-09-25 12:50:50 -0700

    finished most of the logic

--- guessinggame.sh
+++ guessinggame.sh
@@ -2,6 +2,24 @@
 # File: guessinggame.sh
 # Peer graded assignment for Coursera/JHU The Unix Workbench
 
-echo "Guess how many files are in the current directory?"
-read guess
-echo "You entered: $guess"
+count=$(ls -1 | wc -l)
+
+function correct_guess {
+    if [[ $1 -lt $2 ]]
+    then
+	echo "Too low!"
+    elif [[ $1 -gt $2 ]]
+    then
+	echo "Too high!"
+    fi
+}
+
+while [[ $count -ne $guess ]]
+do
+    echo "Guess how many files are in the current directory?"
+    read guess
+    echo "You entered: $guess"
+    echo $(correct_guess $guess $count)
+done
+
+echo "Congratualation! You guessed right!"
